0f324f34c88cd273bfc4e64b976a31d5fd8dcdef
Aakanksha Mahajan <140514405+0002aakansha@users.noreply.github.com>
2023-10-03 12:20:15 +0530

    Delete Node directory

--- Node/backend/controllers/company-controllers.ts
+++
@@ -1,8 +0,0 @@
-const createCompany = async (req, res) => { }
-const getAllListedCompanies = async (req, res) => { }
-const getCompanyByID = async (req, res) => { }
-const updateCompanyByID = async (req, res) => { }
-const deleteCompanyByID = async (req, res) => { }
-
-
-export default { getAllListedCompanies, createCompany, getCompanyByID, updateCompanyByID, deleteCompanyByID }
